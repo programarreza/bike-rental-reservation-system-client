50e52694aaa030107c9de5f9fd5720fee0852e6d
programarreza <programarreza@gmail.com>
2024-08-24 20:48:23 +0600

    initial setup done

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import Container from "../../components/ui/Container/Container";
+
+const Home = () => {
+  return (
+    <div>
+      <Container>
+        <h2>Home Component Coming Soon</h2>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
